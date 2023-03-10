480ca53384b5e5c5b6510dd8db1b7fb775407cd2
alitolibov <alitolibov066@gmail.com>
2023-03-10 15:18:54 +0500

    uptaded apaptive layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,26 +1,33 @@
 import { Outlet } from "react-router-dom";
 import Dropdown from "../components/Dropdown";
-import SearchAppBar from "../components/SearchAppBar";
+
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Layout = () => {
    return ( 
       <>
       <div className="w-[94.3%] m-auto my-[24px] md:w-[96.3%]">
-    <div className="">
-    <div className="w-[132px] h-[70px] bg-contain top-logo m-auto mb-[30px] md:hidden"></div>
-    {/* <div
-        className="h-[50px] w-full flex justify-between items-center px-[12px] bg-[#00AC73] rounded-[15px] mb-[24px] md:hidden">
-        <p className="text-[12px] text-[#ffffff]">Введите название товара или заболевания</p>
-        <div className="search w-[20px] h-[20px]"></div>
-    </div> */}
-    <SearchAppBar/>
-    <div className="flex justify-between h-[50px] mb-[60px] w-full md:hidden">
+    <div className="md:flex md:items-center md:justify-between md:w-full">
+    <div className="w-[132px] bg-no-repeat h-[70px] bg-contain top-logo sm:m-auto mb-[30px] md:mb-[0px] md:h-[50px] md:my-[0px] w-[100px] md:mx-[0px]"></div>
+    <div className="h-[50px] px-[12px] bg-[#00AC73] rounded-[15px] mb-[24px] md:my-[0px]">
+        <form name="form" className="w-full flex items-center gap-[24px] h-full">
+            <input type="text" placeholder="Введите название товара или заболевания" name="search" className="bg-transparent w-full outline-none text-[#ffffff] text-[12px] placeholder:text-[#ffffff]"/>
+            <button>
+              <AiOutlineSearch size={24} className="invert-[100%] md:w-[20px] h-[20px]"/>
+            </button>
+        </form>
+      </div>
+    <div className="flex justify-between h-[50px] mb-[60px] w-full md:justify-start md:hidden">
         <Dropdown/>
-        <div className="h-full flex items-center bg-[#00AC73] rounded-[15px] px-[21px] md:hidden">
+        <div className="h-full flex items-center bg-[#00AC73] rounded-[15px] px-[21px] ">
             <p className="text-[#ffffff]">Войти/Регистрация</p>
         </div>
 
     </div>
+      <div className="hidden md:block"><Dropdown/></div>
+        <div className="h-[50px] hidden items-center bg-[#00AC73] rounded-[15px] px-[21px] md:flex">
+            <p className="text-[#ffffff]">Войти/Регистрация</p>
+        </div>
     </div>
     {/* <div className="hidden md:flex gap-[10px]">
     <div className="w-[122px] h-[65px] bg-contain top-logo m-auto mb-[0px] bg-no-repeat"></div>
